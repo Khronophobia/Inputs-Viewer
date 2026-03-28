43450381e7b2cf130b4cb9af52bef75a93d0cf20
Khronophobia <46954143+Khronophobia@users.noreply.github.com>
2026-03-28 17:01:25 +0700

    make it only display P2 inputs if on two player mode

--- src/hooks/PlayerObject.cpp
+++ src/hooks/PlayerObject.cpp
@@ -5,31 +5,31 @@ using namespace geode::prelude;
 
 struct PlayerObjectHook : Modify<PlayerObjectHook, PlayerObject> {
     bool pushButton(PlayerButton button) {
-        if (!PlayerObject::pushButton(button)) return false;
+        auto result = PlayerObject::pushButton(button);
         auto gameLayer = static_cast<IVGJBaseGameLayer*>(GJBaseGameLayer::get());
-        if (!gameLayer) return true;
-        if (!gameLayer->m_fields->m_ivLayer) return true;
+        if (!gameLayer) return result;
+        if (!gameLayer->m_fields->m_ivLayer) return result;
 
         if (this == gameLayer->m_player1) {
             gameLayer->m_fields->m_ivLayer->handleButton(true, button, true);
-        } else if (this == gameLayer->m_player2) {
+        } else if (this == gameLayer->m_player2 && gameLayer->m_level->m_twoPlayerMode) {
             gameLayer->m_fields->m_ivLayer->handleButton(true, button, false);
         }
 
-        return true;
+        return result;
     }
     bool releaseButton(PlayerButton button) {
-        if (!PlayerObject::releaseButton(button)) return false;
+        auto result = PlayerObject::releaseButton(button);
         auto gameLayer = static_cast<IVGJBaseGameLayer*>(GJBaseGameLayer::get());
-        if (!gameLayer) return true;
-        if (!gameLayer->m_fields->m_ivLayer) return true;
+        if (!gameLayer) return result;
+        if (!gameLayer->m_fields->m_ivLayer) return result;
 
         if (this == gameLayer->m_player1) {
             gameLayer->m_fields->m_ivLayer->handleButton(false, button, true);
-        } else if (this == gameLayer->m_player2) {
+        } else if (this == gameLayer->m_player2 && gameLayer->m_level->m_twoPlayerMode) {
             gameLayer->m_fields->m_ivLayer->handleButton(false, button, false);
         }
         
-        return true;
+        return result;
     }
 };
